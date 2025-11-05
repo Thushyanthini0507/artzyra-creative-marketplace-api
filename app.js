d69186b0669b33645564aa4f1a18e5d7dcb94203
Thushyanthini0507 <thushyanthini0507@gmail.com>
2025-11-05 21:18:30 +0530

    create app.js

---
+++ app.js
@@ -0,0 +1,35 @@
+import cors from "cors";
+import express from "express";
+import connectDB from "./config/db.js";
+// Import routes
+
+// Customer routes
+import customerRoute from "./routes/customerRoutes.js";
+import artistRoute from "./routes/artistRoutes.js";
+import bookingRoute from "./routes/bookingRoutes.js";
+import userRoute from "./routes/authRoutes.js";
+
+// Initialized express
+const app = express();
+
+// Middleware added
+app.use(express.json());
+app.use(cors());
+
+// Test if the server is working or not.
+app.get("/", (req, res) => {
+  res.send("Welcome to Mini pos API");
+});
+
+// Mongo DB connected
+connectDB();
+
+app.use("/api/customers", customerRoute);
+app.use("/api/artists", artistRoute);
+app.use("/api/bookings", bookingRoute);
+app.use("/api/users", userRoute);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server is running in http://localhost:${PORT}`);
+});
